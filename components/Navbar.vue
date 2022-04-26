62aef88bcb4a1125633faf7d587a73b1e7755495
Maxdarkt <tourneuxmaxence@gmail.com>
2022-04-26 16:13:06 +0200

    modify navbar : delete lass active nuxt-link

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,14 +1,15 @@
 <template>
   <nav class="w-fit flex justify-center items-center space-x-8 text-white font-medium">
-    <nuxt-link :to="{ path: '/',hash:'about-me'}" class="transition duration-500 rounded-xl py-2 px-6 hover:bg-white/70 hover:text-black hover:font-bold" exact-active-class="bg-white/70 text-black font-bold">
+    <nuxt-link :to="{ path: '/',hash:'about-me'}" class="transition duration-500 rounded-xl py-2 px-6 hover:bg-white/70 hover:text-black hover:font-bold">
       A PROPOS DE MOI
     </nuxt-link>
-    <nuxt-link :to="{ path: '/',hash:'skills'}" class="transition duration-500 rounded-xl py-2 px-6 hover:bg-white/70 hover:text-black hover:font-bold" exact-active-class="bg-white/70 text-black font-bold">       COMPETENCES
+    <nuxt-link :to="{ path: '/',hash:'skills'}" class="transition duration-500 rounded-xl py-2 px-6 hover:bg-white/70 hover:text-black hover:font-bold">
+      COMPETENCES
     </nuxt-link>
-    <nuxt-link :to="{ path: '/',hash:'portfolio'}" class="transition duration-500 rounded-xl py-2 px-6 hover:bg-white/70 hover:text-black hover:font-bold" exact-active-class="bg-white/70 text-black font-bold">
+    <nuxt-link :to="{ path: '/',hash:'portfolio'}" class="transition duration-500 rounded-xl py-2 px-6 hover:bg-white/70 hover:text-black hover:font-bold">
       PORTFOLIO
     </nuxt-link>
-    <nuxt-link :to="{ path: '/',hash:'contact'}" class="transition duration-500 rounded-xl py-2 px-6 hover:bg-white/70 hover:text-black hover:font-bold" exact-active-class="bg-white/70 text-black font-bold">
+    <nuxt-link :to="{ path: '/',hash:'contact'}" class="transition duration-500 rounded-xl py-2 px-6 hover:bg-white/70 hover:text-black hover:font-bold">
       CONTACT
     </nuxt-link>
   </nav>
